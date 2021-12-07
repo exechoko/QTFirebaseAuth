271dbc4e196faa34eb4af1fe163bae7699a0cb51
Exequiel <49988126+exechoko@users.noreply.github.com>
2021-12-07 16:42:17 -0300

    Add files via upload

---
+++ QtFirebaseAuthenticationExample/main.cpp
@@ -0,0 +1,23 @@
+#include <QCoreApplication>
+#include "authhandler.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    AuthHandler authHandler;
+
+    //Set your API Key here
+    //crear app web en firebase
+    authHandler.setAPIKey("AIzaSyBX4lK4SKiDk43TVCI40qhKEVsmGmMhafU");
+
+    //This is the call to sign a user up using Email/Password
+    //CREAR USUARIO CON EMAIL Y CONTRASEÑA
+    //authHandler.signUserUp("exe@gmail.com", "123456");
+
+    //This is the call to sign an existing user in
+    //INICIAR SESION CON EMAIL Y CONTRASEÑA
+    authHandler.signUserIn("exe@gmail.com", "123456");
+
+    return a.exec();
+}
